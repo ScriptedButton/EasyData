e0645c40eb8131fb106189a69555d9aa1c5f1bf0
Cole Brooks <macoryx@gmail.com>
2018-02-13 10:47:27 -0500

    Update main.py
    update

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 data = input("File where data is stored: ")
 
-content = open(data, 'r').readlines()
+content = open(data, 'r')
+text = content.read()
 
-for i in content:
-  first_name, last_name, dob = i
-  print(first_name, last_name, dob)
-  
-  
+option = input("Read or write: ")
+if option == "read":
+    for i in content.readlines():
+        first_name, last_name, dob = i.split(",")
+        print(first_name, last_name, dob)
+    content.close()
+elif option == "write":
+    values = input("Enter the values: ")
+    file = open(data, 'w')
+    file.write(text + "\n" + values)
+    file.close()
